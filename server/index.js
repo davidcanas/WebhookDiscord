99014b7e914e7d0a081258024a1946f1f85dec7d
David <dg0837.dc@gmail.com>
2022-03-15 19:28:15 +0000

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const bodyparser = require("body-parser")
+
+app.use(bodyparser.json({
+    limit: '50mb'
+}));
+
+app.get("/", (req, res) => {
+
+    res.json({
+        "info": "Website Online"
+    })
+})
+
+app.post("/webhook/enviar", (req, res) => {
+    const Discord = require("discord.js")
+    const webhook = new Discord.WebhookClient({
+        id: req.body.webhookID,
+        token: req.body.webhookToken
+    });
+    webhook.send(req.body.content)
+        .catch(console.error);
+    res.json({
+        "info": "A webhook foi enviada com sucesso."
+    })
+})
+app.listen(3000)
